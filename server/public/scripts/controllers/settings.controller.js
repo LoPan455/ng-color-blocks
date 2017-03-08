dc6de8786e910dc33c542fef947e2a4b6bd4ca8e
Lopan455 <tjohander@gmail.com>
2017-03-08 17:58:31 -0600

    adding color works

--- server/public/scripts/controllers/settings.controller.js
+++ server/public/scripts/controllers/settings.controller.js
@@ -1,5 +1,16 @@
-colorBlocks.controller('SettingsController', function($scope) {
+
+
+colorBlocks.controller('SettingsController',['DataFactory', function(DataFactory) {
   console.log("settings controller running");
 
+ var self = this;
 
-});
+ self.gameColors = DataFactory.gameColors;
+ self.newColor = '';
+
+ self.addColor = function(color) {
+   console.log('submit button clicked');
+  self.gameColors.push(angular.copy(self.newColor));
+ }
+
+}]);
--- server/public/scripts/factories/data.factory.js
+++ server/public/scripts/factories/data.factory.js
@@ -1,16 +1,14 @@
 // this is the empty factory referenced in the instructions
 
-colorBlocks.factory('DataFactory', [function() {
+colorBlocks.factory('DataFactory',function() {
   console.log("data factory running");
   var colors = ['red', 'blue', 'magenta', 'green', 'pink'];
-
-
-
-
+  console.log('DataFactory Colors are: ',colors);
 
   return {
-    gameColors: colors
+    gameColors: colors,
+    testText: 'test from DataFactory text'
   }
 
 
-}]);
+});
