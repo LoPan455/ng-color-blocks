6ad86cb97c13fea25a490d292018aefa9ec93733
thomas <tjohander@gmail.com>
2017-03-09 00:25:11 -0600

    cleaned up code, made comments

--- server/public/scripts/controllers/game.controller.js
+++ server/public/scripts/controllers/game.controller.js
@@ -7,20 +7,15 @@ console.log('game controller running');
 var self = this;
 // This is where we will bring in our factory 'exports'
 self.colors = DataFactory.gameColors //and array of objects
-self.currentScore = 0;
-self.currentPlayer = {};
-self.players = DataFactory.gamePlayers;
-
-
-
-
+self.currentScore = 0; //holds the current score
+self.currentPlayer = {}; //temporary storage for the current player name and score
+self.players = DataFactory.gamePlayers; //brings in the player 'database'
 
 // start game
 init();
 
 // resets game to the starting state
 function init() {
-  console.log('self.players is: ',self.players);
   self.messageText = "";
   self.currentColor = self.colors[randomNumber(0, self.colors.length - 1)];
   self.colorPrompt = 'Can you find the ' + self.currentColor.name + ' block?'
@@ -29,13 +24,15 @@ function init() {
 // click handler for guessing colors
 self.handleInput = function(clickedColor) {
   if(clickedColor === self.currentColor) {
+    //increments the score
     self.currentScore++ ;
     alert('You got it!\n\nNow try another!');
+    //writes the score to the current player object
     self.currentPlayer.score = self.currentScore;
     init();
   } else {
+    //pushes the current player into the array of past players..Our makeshift db
     self.players.push(self.currentPlayer);
-    console.log(self.players);
     self.messageText = 'Oh no! You guessed wrong!';
   }
 }
--- server/public/scripts/factories/data.factory.js
+++ server/public/scripts/factories/data.factory.js
@@ -3,32 +3,24 @@
 colorBlocks.factory('DataFactory',function() {
 
   var colors = [{name:'red'},{name:'purple'},{name:"AliceBlue"},{name:"Green"},{name:"Aqua"},{name:"Aquamarine"},{name:"pink"},{name:"Beige"},{name:"Black"}];
-  var players = [{name: 'Donald Bagel', score: 145},{name: 'Carolyn', score: 3015}];
-  var highScore = 0;
-  var scoresArray = [];
+  var players = [{name: 'Donald Bagel', score: 145},{name: 'Carolyn', score: 3015}];  // the databse of players
 
-  // var highScoreFinder = function() {
-  //   console.log('highscore finder() hits');
-  //   for (var i = 0; i < players.length; i++) {
-  //     scoresArray.push(players.score);
-  //   }
-  //   scoresArray.sort()
-  //   highScore = sortingContainer[sortingContainer.length - 1];
-  //   return highScore;
-  // }
 
+  // finds the high score among all past players in the 'database'
   var highScoreFinder = function (array){
       console.log('highscore finder() hits');
+      //sets a temporary holding spot to collect all past scores
       var container = [];
       for (var i = 0; i < array.length; i++) {
         container.push(array[i].score)
       }
-     return container.pop();
-    }
+      //sorts the container so the largest value is at the end
+      container.sort();
+      //pops the largest value off the end and returns it out of the function
+      return container.pop();
+      }
 
-
-
-
+  //our factory 'exports'
   return {
     gameColors: colors,
     gamePlayers: players,
